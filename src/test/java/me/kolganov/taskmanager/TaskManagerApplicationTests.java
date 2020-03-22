fdd580b543109646f4b357a7d5f62de0687694da
roman <ramoni1990@gmail.com>
2020-03-22 22:08:30 +0300

    init

---
+++ src/test/java/me/kolganov/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kolganov.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
