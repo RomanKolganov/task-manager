f05ece003204968ce4419434f1a5e417797b5632
roman <ramoni1990@gmail.com>
2020-03-26 10:24:07 +0300

    deploy heroku 4

---
+++ src/main/resources/static/precache-manifest.47239f22925f2570c0f9d200d411b877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd6b2c3205c558fda279cad4121d97f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f22adf521fb28a243d8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "add0096770f78ca57ece",
+    "url": "/static/css/main.d242b985.chunk.css"
+  },
+  {
+    "revision": "9f22adf521fb28a243d8",
+    "url": "/static/js/2.b1f05287.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b1f05287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add0096770f78ca57ece",
+    "url": "/static/js/main.1d9e731f.chunk.js"
+  },
+  {
+    "revision": "07de2ef65dc3870f458f",
+    "url": "/static/js/runtime-main.fd6e0d7a.js"
+  }
+]);
