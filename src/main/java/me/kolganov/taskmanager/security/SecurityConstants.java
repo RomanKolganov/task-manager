24cc7aba16904f77c49d5e35b29fb682718b3184
roman <ramoni1990@gmail.com>
2020-03-26 10:06:39 +0300

    deploy heroku 3

--- src/main/java/me/kolganov/taskmanager/security/SecurityConstants.java
+++ src/main/java/me/kolganov/taskmanager/security/SecurityConstants.java
@@ -4,5 +4,5 @@ public class SecurityConstants {
     public final static String SECRET = "SecretKeyForJWTs";
     public final static String TOKEN_PREFIX = "Bearer ";
     public final static String HEADER_STRING = "Authorization";
-    public final static long EXPIRATION_TIME = 500000;
+    public final static long EXPIRATION_TIME = 3000000; //50 min
 }
